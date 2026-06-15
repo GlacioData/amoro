b722e4b379513aa228ade159e3086b8fed977246
ZhouJinsong <zhoujinsong0505@163.com>
2026-06-15 10:56:17 +0800

    Revert "[AMORO-4246] Refactor process data expiring via ProcessFactory plugin" (#4252)
    Revert "[AMORO-4246] Refactor process data expiring via ProcessFactory plugin…"
    
    This reverts commit acf8ef47a3d9305ef217399a9aeb8c6d312bbe75.
    
    (cherry picked from commit 374ed06bdb3d2de20856f6e2f13b4a5c49cc4757)

--- amoro-ams/src/main/java/org/apache/amoro/server/AmoroServiceContainer.java
+++ amoro-ams/src/main/java/org/apache/amoro/server/AmoroServiceContainer.java
@@ -291,6 +291,7 @@ public class AmoroServiceContainer {
     addHandlerChain(optimizingService.getTableRuntimeHandler());
     addHandlerChain(processService.getTableHandlerChain());
     addHandlerChain(InlineTableExecutors.getInstance().getOptimizingCommitExecutor());
+    addHandlerChain(InlineTableExecutors.getInstance().getProcessDataExpiringExecutor());
     addHandlerChain(InlineTableExecutors.getInstance().getBlockerExpiringExecutor());
     addHandlerChain(InlineTableExecutors.getInstance().getTableRefreshingExecutor());
     tableService.initialize();
